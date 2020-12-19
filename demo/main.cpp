02a08bd8b658ad1704725994387c03d1c045df21
Ekaterina Deynega <deynega.katena@mail.ru>
2020-12-19 17:27:16 +0300

    documents list && parsed information

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,5 +1,10 @@
-#include <header.hpp>
+  #include <Analyzer.hpp>
+  #include <iostream>
+
 
 int main() {
-
+  std::string path = "/home/ekaterina/CLionProjects/lab-04-filesystem/misc/ftp";
+  Analyzer t(path);
+  t.analyze_directory(path, std::cout);
+  t.show_information(std::cout);
 }
