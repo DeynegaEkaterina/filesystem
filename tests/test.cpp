b45780fef1b2495c4f7ac10e93ee389b876f32f7
Ekaterina Deynega <deynega.katena@mail.ru>
2020-12-21 19:31:50 +0300

    final version

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,14 +1,8 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <gtest/gtest.h>
-#include "Analyzer.hpp"
-#include <boost/filesystem.hpp>
 
-TEST(Example, Check) {
-    boost::filesystem::path path_to_ftp =
-        "/home/ekaterina/CLionProjects/lab-04-filesystem/misc/ftp";
-    Analyzer test(path_to_ftp);
-    test.show_information(std::cout);
-    test.analyze_directory(path_to_ftp, std::cout);
+TEST(Example, EmptyTest) {
+
 EXPECT_TRUE(true);
 }
