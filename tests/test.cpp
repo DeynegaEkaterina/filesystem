2672a2a1f1e5d602469783dbea11b8a0912c2fdc
Ekaterina Deynega <deynega.katena@mail.ru>
2020-12-21 19:09:22 +0300

    shows the work through tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,14 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "Analyzer.hpp"
+#include <boost/filesystem.hpp>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Example, Check) {
+    boost::filesystem::path path_to_ftp =
+        "/home/ekaterina/CLionProjects/lab-04-filesystem/misc/ftp";
+    Analyzer test(path_to_ftp);
+    test.show_information(std::cout);
+    test.analyze_directory(path_to_ftp, std::cout);
+EXPECT_TRUE(true);
 }
